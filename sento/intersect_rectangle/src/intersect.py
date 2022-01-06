0cd8378072675dc00f828f64c0a9e10012968294
Oleg Parashchenko <olpa@uucode.com>
2022-01-06 07:27:19 +0100

    Validate rectangle pair.

--- sento/intersect_rectangle/src/intersect.py
+++ sento/intersect_rectangle/src/intersect.py
@@ -1,7 +1,26 @@
 import json
+import collections.abc
+import numbers
+
+
+def validate_rect(rect) -> None:
+    if not isinstance(rect, collections.abc.Mapping):
+        raise ValueError("Rectange should be a mapping")
+    for field in ("x1", "y1", "x2", "y2"):
+        val = rect.get(field, None)
+        if not isinstance(val, numbers.Real):
+            raise ValueError(f"The field '{field}' should be a real number")
+
+
+def validate_rect_pair(rect_pair) -> None:
+    if not isinstance(rect_pair, collections.abc.Mapping):
+        raise ValueError("Rectange pair should be a mapping")
+    validate_rect(rect_pair.get("rectangle1", None))
+    validate_rect(rect_pair.get("rectangle2", None))
 
 
 def has_rectangle_intersection(rect_pair) -> bool:
+    validate_rect_pair(rect_pair)
     return rect_pair["rectangle2"]["y1"] < 0
 
 
--- sento/intersect_rectangle/test/intersect_test.py
+++ sento/intersect_rectangle/test/intersect_test.py
@@ -1,9 +1,10 @@
 import unittest
-from hamcrest import assert_that, is_not, calling, raises
+from hamcrest import assert_that, is_not, described_as, calling, raises
 
 from lib import has_rectangle_intersection_jsonstr_matcher
 
-from intersect import has_rectangle_intersection_jsonstr
+from intersect import has_rectangle_intersection_jsonstr, \
+    has_rectangle_intersection
 
 
 class HasIntersectionJsonstrTest(unittest.TestCase):
@@ -47,6 +48,48 @@ class HasIntersectionJsonstrTest(unittest.TestCase):
         assert_that(
                 calling(has_rectangle_intersection_jsonstr).with_args(s_json),
                 raises(ValueError))
+
+
+class HasIntersectionTest(unittest.TestCase):
+
+    def test_validate_input(self):
+        valid_rect = {
+                "x1": 1, "y1": 2, "x2": 1.2, "y2": 1.3
+                }
+        fixture_set = [
+                "dummy", "Not a dict",
+                {}, "Not a dict: 'rectangle1' (missed)",
+                {
+                    "rectangle1": valid_rect,
+                    "rectangle2": []
+                }, "Not a dict: 'rectangle2'",
+                {
+                    "rectangle1": {**valid_rect, "x1": None},
+                    "rectangle2": valid_rect
+                }, "Bad field on 'rectangle1' ('x1')",
+                {
+                    "rectangle1": valid_rect,
+                    "rectangle2": {**valid_rect, "y1": "not-a-number"}
+                }, "Bad field on 'rectangle2' ('y1')",
+                {
+                    "rectangle1": valid_rect,
+                    "rectangle2": {**valid_rect, "x2": 4+2j}
+                }, "Bad field 'x2' (not a real number)",
+                {
+                    "rectangle1": {**valid_rect, "y2": "2"},
+                    "rectangle2": valid_rect
+                }, "Bad field 'y2'",
+                ]
+
+        def pairwise(t):
+            it = iter(t)
+            return zip(it, it)
+
+        for (fixture, describe) in pairwise(fixture_set):
+            assert_that(
+                    calling(has_rectangle_intersection).with_args(fixture),
+                    described_as(describe,
+                                 raises(ValueError)))
 
 
 if '__main__' == __name__:
