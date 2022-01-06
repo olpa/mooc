67c3341d8bad9660885552115de6c6a0a80a63d5
Oleg Parashchenko <olpa@uucode.com>
2022-01-06 06:47:19 +0100

    Add test for "no intersection".

--- sento/intersect_rectangle/src/intersect.py
+++ sento/intersect_rectangle/src/intersect.py
@@ -1,6 +1,10 @@
 import json
 
 
+def has_rectangle_intersection(rect_pair) -> bool:
+    return rect_pair["rectangle2"]["y1"] < 0
+
+
 def has_rectangle_intersection_jsonstr(s_json: str) -> bool:
     rect_pair = json.loads(s_json)
-    return bool(rect_pair)
+    return has_rectangle_intersection(rect_pair)
--- sento/intersect_rectangle/test/intersect_test.py
+++ sento/intersect_rectangle/test/intersect_test.py
@@ -1,5 +1,5 @@
 import unittest
-from hamcrest import assert_that, calling, raises
+from hamcrest import assert_that, is_not, calling, raises
 
 from lib import has_rectangle_intersection_jsonstr_matcher
 
@@ -8,7 +8,7 @@ from intersect import has_rectangle_intersection_jsonstr
 
 class HasIntersectionJsonstrTest(unittest.TestCase):
 
-    def test_with_json_as_string(self):
+    def test_has_intersection(self):
         s_json = '''
         {
           "rectangle1": {
@@ -23,6 +23,23 @@ class HasIntersectionJsonstrTest(unittest.TestCase):
         '''
 
         assert_that(s_json, has_rectangle_intersection_jsonstr_matcher())
+
+    def test_has_no_intersection(self):
+        s_json = '''
+        {
+          "rectangle1": {
+            "x1": -2.3, "y1": -4.5,
+            "x2": -1.5, "y2": -2.2
+          },
+          "rectangle2": {
+            "x1": 12.3, "y1": 14.5,
+            "x2": 10.5, "y2": 12.2
+          }
+        }
+        '''
+
+        assert_that(s_json,
+                    is_not(has_rectangle_intersection_jsonstr_matcher()))
 
     def test_fail_on_unparsable_json(self):
         s_json = '''{ ###'''
