f25fdfb4ad3f0aab8f144520fb25f7fc40b73c83
Oleg Parashchenko <olpa@uucode.com>
2025-06-28 18:56:27 +0200

    Fetch and run own agent.

---
+++ agents/hugging-face/unit21/060_run_own.py
@@ -0,0 +1,9 @@
+from smolagents import CodeAgent, InferenceClientModel
+
+agent = CodeAgent(tools=[], model=InferenceClientModel())
+
+
+# Doesn't work because uses HfApiModel
+alfred_agent = agent.from_hub('olpa/AlfredAgent', trust_remote_code=True)
+
+alfred_agent.run("Give me the best playlist for a party at Wayne's mansion. The party idea is a 'villain masquerade' theme")  
