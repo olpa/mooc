f96d8b419c38d1c2b25b9b101154c93e42f8205c
Oleg Parashchenko <olpa@uucode.com>
2025-06-29 22:20:20 +0200

    Recognize person features on an image.

---
+++ agents/hugging-face/unit21/400_images_to_features.py
@@ -0,0 +1,39 @@
+from PIL import Image
+import requests
+from io import BytesIO
+
+image_urls = [
+    "https://upload.wikimedia.org/wikipedia/commons/e/e8/The_Joker_at_Wax_Museum_Plus.jpg", # Joker image
+    "https://upload.wikimedia.org/wikipedia/en/9/98/Joker_%28DC_Comics_character%29.jpg" # Joker image
+]
+
+images = []
+for url in image_urls:
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36" 
+    }
+    response = requests.get(url,headers=headers)
+    image = Image.open(BytesIO(response.content)).convert("RGB")
+    images.append(image)
+
+from smolagents import CodeAgent, OpenAIServerModel
+import os
+
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+model = OpenAIServerModel(model_id="gpt-4o", api_key=OPENAI_API_KEY)
+
+# Instantiate the agent
+agent = CodeAgent(
+    tools=[],
+    model=model,
+    max_steps=20,
+    verbosity_level=2
+)
+
+response = agent.run(
+    """
+    Describe the costume and makeup that the comic character in these photos is wearing and return the description.
+    Tell me if the guest is The Joker or Wonder Woman.
+    """,
+    images=images
+)
