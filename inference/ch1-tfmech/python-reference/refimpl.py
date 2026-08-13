dc419b8a3baaeebf26764c635c63a0b19b86778d
Oleg Parashchenko <olpa@uucode.com>
2026-08-13 10:50:03 +0200

    Create tests for causal_attenstion.

--- inference/ch1-tfmech/python-reference/refimpl.py
+++ inference/ch1-tfmech/python-reference/refimpl.py
@@ -8,3 +8,11 @@ def attention(q, k, v):
     scores = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(d_k)
     weights = torch.softmax(scores, dim=-1)
     return torch.matmul(weights, v)
+
+
+def causal_attention(q, k, v):
+    d_k = q.shape[-1]
+    scores = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(d_k)
+    mask = torch.triu(torch.ones(scores.shape[-1], scores.shape[-1]), diagonal=1).bool()
+    scores = scores.masked_fill(mask, float('-inf'))
+    return torch.matmul(torch.softmax(scores, dim=-1), v)
