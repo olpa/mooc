fd1ce22b485dfa218e8cdcc6519e602ea38bed38
Oleg Parashchenko <olpa@uucode.com>
2026-08-14 09:54:38 +0200

    MultiHeadAttention, struct and new.

--- inference/ch1-tfmech/src/lib.rs
+++ inference/ch1-tfmech/src/lib.rs
@@ -1,5 +1,5 @@
 use candle_core::{DType, Device, Result, Tensor};
-use candle_nn::ops::softmax;
+use candle_nn::{linear_no_bias, ops::softmax, Linear, VarBuilder};
 
 // q, k: (batch_size, seq_len, head_dim)
 // v: (batch_size, seq_len, layer_dim)
@@ -20,9 +20,33 @@ pub fn causal_attention(q: &Tensor, k: &Tensor, v: &Tensor) -> Result<Tensor> {
     let seq_len = scores.dim(1)?;
     let bool_mask = Tensor::tril2(seq_len, DType::U8, &Device::Cpu)?;
     let zero = Tensor::new(0f32, &Device::Cpu)?.broadcast_as((seq_len, seq_len))?;
-    let minus_inf = Tensor::new(f32::NEG_INFINITY, &Device::Cpu)?.broadcast_as((seq_len, seq_len))?;
+    let minus_inf =
+        Tensor::new(f32::NEG_INFINITY, &Device::Cpu)?.broadcast_as((seq_len, seq_len))?;
     let additive_mask = bool_mask.where_cond(&zero, &minus_inf)?;
     let masked_scores = scores.broadcast_add(&additive_mask)?;
     let weights = softmax(&masked_scores, 2)?; // (batch_size, seq_len, seq_len)
     weights.matmul(&v)
+}
+
+struct MultiHeadAttention {
+    num_heads: usize,
+    head_dim: usize,
+    q_proj: Linear,
+    k_proj: Linear,
+    v_proj: Linear,
+    out_proj: Linear,
+}
+
+impl MultiHeadAttention {
+    pub fn new(hidden_dim: usize, num_heads: usize, vb: &mut VarBuilder) -> Result<Self> {
+        let head_dim = hidden_dim / num_heads;
+        Ok(Self {
+            num_heads,
+            head_dim,
+            q_proj: linear_no_bias(hidden_dim, hidden_dim, vb.push_prefix("q_proj"))?,
+            k_proj: linear_no_bias(hidden_dim, hidden_dim, vb.push_prefix("k_proj"))?,
+            v_proj: linear_no_bias(hidden_dim, hidden_dim, vb.push_prefix("v_proj"))?,
+            out_proj: linear_no_bias(hidden_dim, hidden_dim, vb.push_prefix("out_proj"))?,
+        })
+    }
 }
