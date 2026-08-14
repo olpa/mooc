4edbf8a9ff722950948999c0c1581d8fd96cdab5
Oleg Parashchenko <olpa@uucode.com>
2026-08-14 12:10:28 +0200

    My version of "forward".

--- inference/ch1-tfmech/src/lib.rs
+++ inference/ch1-tfmech/src/lib.rs
@@ -1,5 +1,5 @@
 use candle_core::{DType, Device, Result, Tensor};
-use candle_nn::{linear_no_bias, ops::softmax, Linear, VarBuilder};
+use candle_nn::{linear_no_bias, ops::softmax, Linear, Module, VarBuilder};
 
 // q, k: (batch_size, seq_len, head_dim)
 // v: (batch_size, seq_len, layer_dim)
@@ -48,5 +48,17 @@ impl MultiHeadAttention {
             v_proj: linear_no_bias(hidden_dim, hidden_dim, vb.push_prefix("v_proj"))?,
             out_proj: linear_no_bias(hidden_dim, hidden_dim, vb.push_prefix("out_proj"))?,
         })
+    }
+}
+
+impl Module for MultiHeadAttention {
+    // (batch_size, seq_len, hidden_dim) -> -> (batch_size, seq_len, n_heads, head_dim) (batch_size, seq_len, hidden_dim)
+    fn forward(&self, x: &Tensor) -> Result<Tensor> {
+        let q = self.q_proj.forward(x)?.unfold(2, self.head_dim, 1)?;
+        let k = self.k_proj.forward(x)?.unfold(2, self.head_dim, 1)?;
+        let v = self.v_proj.forward(x)?.unfold(2, self.head_dim, 1)?;
+        let out = causal_attention(&q, &k, &v)?;
+        let out = out.flatten_from(2)?;
+        self.out_proj.forward(&out)
     }
 }
