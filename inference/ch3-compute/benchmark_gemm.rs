bbdedadcced9b69497a7ba70018c275e9cfe6632
Oleg Parashchenko <olpa@uucode.com>
2026-09-10 10:09:50 +0200

    My first Rust version of GEMM.

--- inference/ch3-compute/benchmark_gemm.rs
+++ inference/ch3-compute/benchmark_gemm.rs
@@ -1,3 +1,24 @@
+use std::{hint::black_box, time::Instant};
+
+use candle_core::Tensor;
+
+fn benchmark_gemm(m: usize, n: usize, k: usize) -> candle_core::Result<f32> {
+    let cuda = candle_core::Device::new_cuda(0)?;
+    let a = Tensor::rand(0.0, 1.0, &[m, k], &cuda)?;
+    let b = Tensor::rand(0.0, 1.0, &[k, n], &cuda)?;
+    cuda.synchronize()?;
+    let start = Instant::now();
+    for _ in 0..100 {
+        let dummy = a.matmul(&b)?;
+        let _ = black_box(dummy);
+    }
+    cuda.synchronize()?;
+    return Ok(((2 * m * n * k + 100) as f32) / (start.elapsed().as_millis() as f32) / 1e12);
+}
+
 fn main() {
-    println!("sample1");
+    for d in [512, 1024, 2048, 4096, 6144, 8192] {
+        let perf = benchmark_gemm(d, d, d).unwrap();
+        println!("{d}x{d}: {perf} TFLOPS");
+    }
 }
