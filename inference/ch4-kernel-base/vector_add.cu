794519ae07bd2b2019fd2e806e08d0a055123bd7
Oleg Parashchenko <olpa@uucode.com>
2026-09-15 09:32:28 +0200

    basic vector_add

---
+++ inference/ch4-kernel-base/vector_add.cu
@@ -0,0 +1,6 @@
+__global__ void vector_add(float *a, float *b, float *c, int n) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < n) {
+        c[idx] = a[idx] + b[idx];
+    }
+}
